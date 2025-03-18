8de8e1ecd84146fe3a30801cae7287aeb5eb871a
miruomoo <mlin282@uwo.ca>
2025-03-18 14:52:03 -0400

    Added Slime Movement

--- Assets/Scripts/PlayerController2D.cs
+++ Assets/Scripts/PlayerController2D.cs
@@ -7,6 +7,8 @@ public class PlayerController2D : MonoBehaviour
     public float jumpForce = 10f;
     private Rigidbody2D rb;
     private bool isGrounded;
+    private int jumpCount = 0;     // Track number of jumps
+    public int maxJumps = 1;       // Maximum number of jumps allowed
 
     public Transform groundCheck;  // Empty GameObject under the player
     public LayerMask groundLayer;  // Layer to detect ground
@@ -26,10 +28,17 @@ public class PlayerController2D : MonoBehaviour
         // Move the player
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
 
+        // Check if grounded and reset jump count
+        if (IsGrounded())
+        {
+            jumpCount = 0;
+        }
        
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        // Jump when button pressed and either grounded or have jumps remaining
+        if (Input.GetButtonDown("Jump") && jumpCount < maxJumps)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpCount++;
         }
 
         
---
+++ Assets/Scripts/SlimeMove.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeMove : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] private float moveDistance = 5f;
+    
+    private Vector3 startPosition;
+    private bool movingRight = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Calculate movement direction
+        float direction = movingRight ? 1f : -1f;
+        
+        // Move the enemy
+        transform.Translate(Vector3.right * direction * moveSpeed * Time.deltaTime);
+        
+        // Check if we need to change direction
+        if (movingRight && transform.position.x >= startPosition.x + moveDistance)
+        {
+            movingRight = false;
+        }
+        else if (!movingRight && transform.position.x <= startPosition.x)
+        {
+            movingRight = true;
+        }
+    }
+}
