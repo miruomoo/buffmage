22c947b56fde8c7318b282a73b4fd18975301861
miruomoo <mlin282@uwo.ca>
2025-03-26 20:20:40 -0400

    Added health level 1

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 3;
+    private int currentHealth;
+    
+    // Array of health images (hearts)
+    public GameObject[] healthImages = new GameObject[3];
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        UpdateHealthDisplay();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if the colliding object is an enemy
+        if (other.CompareTag("Enemy"))
+        {
+            TakeDamage();
+        }
+    }
+
+    private void TakeDamage()
+    {
+        currentHealth--;
+        UpdateHealthDisplay();
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void UpdateHealthDisplay()
+    {
+        // Deactivate health images based on current health
+        for (int i = 0; i < healthImages.Length; i++)
+        {
+            if (healthImages[i] != null)
+            {
+                // Show image if i is less than currentHealth
+                healthImages[i].SetActive(i < currentHealth);
+            }
+        }
+    }
+
+    private void Die()
+    {
+        // Restart the scene when player dies
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
