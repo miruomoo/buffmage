cda7f57a936bd695d2159e75a996dfbd2cffcce3
miruomoo <mlin282@uwo.ca>
2025-03-29 23:17:33 -0400

    Fixed some bugs

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -14,6 +14,7 @@ public class PlayerHealth : MonoBehaviour
 
     void Start()
     {
+        Time.timeScale = 1f;
         currentHealth = maxHealth;
         UpdateHealthDisplay();
         deathMenuPanel.SetActive(false);
